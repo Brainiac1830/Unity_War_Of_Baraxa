6039b72c706cd778a2c8e64122a8a4971e0c5b91
Ekmort <ydmule@hotmail.com>
2014-10-01 11:04:26 -0400

    Blinking Label
    blink du text appuyez sur un touche

--- Wars Of Baraxa/Assets/Scripts/script_Lobby.js
+++ Wars Of Baraxa/Assets/Scripts/script_Lobby.js
@@ -1,8 +1,12 @@
 ﻿#pragma strict
 var Lobby : GUIStyle;
+var warOfBaraxa : GUIStyle;
 
 
 function OnGUI () {
+	warOfBaraxa.fontSize = Screen.width/10;
+
+	GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.6f/2), Screen.height * 0.1f,Screen.width * 0.6f, Screen.height * 0.1f), "Wars of Baraxa", warOfBaraxa);
 	GUI.Box(Rect(Screen.width/4.2, Screen.height/3,400, 300),"Liste des parties");
 	
 	if(GUI.Button(Rect(Screen.width/3.6, Screen.height/1.1,100, 30),"Créer")){
