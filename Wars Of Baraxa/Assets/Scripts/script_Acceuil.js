e634545904f2cc5ee55390396a0f48cde4ece31f
OMimeault <olivierbmimeault@gmail.com>
2014-10-01 10:27:00 -0400

    Interface dynamique
    #PasDeLobby
    #OptionQuitterÀFinir

--- Wars Of Baraxa/Assets/Scripts/script_Acceuil.js
+++ Wars Of Baraxa/Assets/Scripts/script_Acceuil.js
@@ -1,10 +1,16 @@
-﻿var test : GUIStyle;
-
-function OnGUI(){
-	GUI.Button(Rect(Screen.width/5, Screen.height / 1.1,300, 50), "Appuyer sur une touche pour continuer", test);
-}
+﻿var appuyerTouche : GUIStyle;
+var warOfBaraxa : GUIStyle;
 
 function Update() {
 		if(Input.anyKeyDown)
 			Application.LoadLevel("Menu");
+	}
+
+
+function OnGUI(){
+	appuyerTouche.fontSize = Screen.width/30;
+	warOfBaraxa.fontSize = Screen.width/10;
+	GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.6f/2), Screen.height * 0.8f, Screen.width * 0.6f, Screen.height *0.2f), "Appuyer sur une touche pour continuer", appuyerTouche);
+	GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.6f/2), Screen.height * 0.1f,Screen.width * 0.6f, Screen.height * 0.1f), "Wars of Baraxa", warOfBaraxa);
 }
+
--- Wars Of Baraxa/Assets/Scripts/script_Connexion.js
+++ Wars Of Baraxa/Assets/Scripts/script_Connexion.js
@@ -1,46 +1,80 @@
-﻿var alias : String = "";
+﻿//Variables
+var alias : String = "";
 var password : String = "";
 var nom : String = "";
 var prenom : String = "";
-var test1 : GUIStyle;
-var test2 : GUIStyle;
-var newAccount = false;
-var hasBeenPressed = false;
+var nouveauCompte = false;
+//GUIStyle
+var textArea : GUIStyle;
+var buttons : GUIStyle;
+var text : GUIStyle;
+var warOfBaraxa : GUIStyle;
+
+
 
 function OnGUI() {
-		// Make a text field that modifies stringToEdit.
-			GUI.Label(Rect(Screen.width/2 - 131, 200, 50, 20), "Alias");
-			alias = GUI.TextField (Rect (Screen.width/2 - 100, 200, 260, 20), alias, 25, test1);
-			GUI.Label(Rect(Screen.width/2 - 182, 230, 100, 20), "Mot de passe");
-			password = GUI.TextField (Rect (Screen.width/2 - 100, 230, 260, 20), password, 25, test1);
+	warOfBaraxa.fontSize = Screen.width / 10;
+	text.fontSize = Screen.width/45;
+	textArea.fontSize = Screen.width/45;
+	buttons.fontSize = Screen.width/45;
+	
+	//Titre
+	GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.6f/2), Screen.height * 0.1f,Screen.width * 0.6f, Screen.height * 0.1f), "Wars of Baraxa", warOfBaraxa);
+	//Alias
+	GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.40f/2), Screen.height * 0.40f, Screen.width * 0.10f, Screen.height * 0.05f), "Alias", text);
+	alias = GUI.TextField (Rect ((Screen.width/2) - (Screen.width * 0.25f/2), Screen.height * 0.40f, Screen.width * 0.25f, Screen.height * 0.05f), alias, 20, textArea);
+	//Mot de passe
+	GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.485f/2), Screen.height * 0.47f, Screen.width * 0.10f, Screen.height * 0.05f), "Mot de passe", text);
+	password = GUI.PasswordField (Rect ((Screen.width/2) - (Screen.width * 0.25f/2), Screen.height * 0.47f, Screen.width * 0.25f, Screen.height * 0.05f),password, "*"[0] ,20, textArea);
+	
+	//Si veut se connecter a un compte
+	if(!nouveauCompte){
+		//Tente de se connecter avec les informations fournis
+		if(GUI.Button(Rect((Screen.width * 0.435f) - (Screen.width * 0.12f/2), Screen.height * 0.54f, Screen.width * 0.12f, Screen.height * 0.06f), "Connecter", buttons)){
+			//Conditions d'erreurs
+			if(alias == "" || password == ""){
+				//Erreur
+			}
+			else if (false){
+				//Code pour vérifier si l'alias et le mot de passe 
+				//correspondent a un enregistrement dans la base de donnée
+			}
+			else{
+				//Connection et changement de page
+			}
 			
-			if(!hasBeenPressed){
-				if(GUI.Button(Rect(Screen.width/2 -100, 260, 120, 30), "Connecter", test2)){
-					//Code pour vérifier si l'alias et le mot de passe 
-					//correspondent a un enregistrement dans la base de donnée
-				}
+		}
+		//Change l'interface pour pouvoir créer un compte
+		if(GUI.Button(Rect((Screen.width * 0.564) - (Screen.width * 0.12f/2), Screen.height * 0.54f, Screen.width * 0.12f, Screen.height * 0.06f), "Créer", buttons)){
+			nouveauCompte = true;
+		}
+	}
+	//Si on veut créer un nouveau compte (plus de champs a remplir et boutons changent de place
+	if(nouveauCompte){
+		//Prenom
+		GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.43f/2), Screen.height * 0.54f, Screen.width * 0.10f, Screen.height * 0.05f), "Prenom", text);
+		prenom = GUI.TextField(Rect((Screen.width/2) - (Screen.width * 0.25f/2), Screen.height * 0.54f, Screen.width * 0.25f, Screen.height * 0.05f), prenom, 25, textArea);
+		//Nom
+		GUI.Label(Rect((Screen.width/2) - (Screen.width * 0.40f/2), Screen.height * 0.61f, Screen.width * 0.10f, Screen.height * 0.05f), "Nom", text);
+		nom = GUI.TextField(Rect((Screen.width/2) - (Screen.width * 0.25f/2), Screen.height * 0.61f, Screen.width * 0.25f, Screen.height * 0.05f), nom, 25, textArea);
 		
-				if(GUI.Button(Rect(Screen.width/2 + 40, 260, 120, 30), "Nouvel utilisateur", test2)){
-					newAccount = true;
-					hasBeenPressed = true;
-				}
+		//Si on click sur "RETOUR" on retourne a la connection d'un compte existant
+		if(GUI.Button(Rect((Screen.width * 0.435) - (Screen.width * 0.12f/2), Screen.height * 0.68f, Screen.width * 0.12f, Screen.height * 0.05f), "Retour", buttons)){
+			if(alias == "" || password == "" || nom == "" || prenom == ""){
+				//Erreur
+			}
+			else if(false){
+				//Alias déja utilisé
+				//Erreur
 			}
 			
-			if(newAccount){
-				GUI.Label(Rect(Screen.width/2 - 130, 260, 100, 20), "Nom");
-				prenom = GUI.TextField (Rect (Screen.width/2 - 100, 260, 260, 20), prenom, 25, test1);
-				GUI.Label(Rect(Screen.width/2 - 148, 290, 100, 20), "Prenom");
-				nom = GUI.TextField (Rect (Screen.width/2 - 100, 290, 260, 20), nom, 25, test1);
-				
-				if(GUI.Button(Rect(Screen.width/2 - 100, 320, 120, 30), "Retour", test2)){
-					newAccount = false;
-					hasBeenPressed = false;
-				}
-				
-				if(GUI.Button(Rect(Screen.width/2 + 40, 320, 120, 30), "Créer", test2)){
-					//Code pour ajouter un enregistrement dans la base de donnée 
-					//si l'alias n'est pas déja utilisé
-				}
-			}
+			nouveauCompte = false;
+		}
+		//Si on click sur "CRÉER" on tente de créer le compte selon les informations données (vérifications)
+		if(GUI.Button(Rect((Screen.width * 0.564) - (Screen.width * 0.12f/2), Screen.height * 0.68f, Screen.width * 0.12f, Screen.height * 0.05f), "Créer", buttons)){
+			//Code pour ajouter un enregistrement dans la base de donnée 
+			//si l'alias n'est pas déja utilisé
+		}
+	}
 }		
 
