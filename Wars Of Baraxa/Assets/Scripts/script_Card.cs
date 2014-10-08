5ff15151a98e3f738a49941097853d5950245078
OMimeault <olivierbmimeault@gmail.com>
2014-10-08 17:37:44 -0400

    fk off
    asdf

---
+++ Wars Of Baraxa/Assets/Scripts/script_Card.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_Card : MonoBehaviour {
+
+	/*string nom_ = "";
+	int vie_ = 0;
+	int armure_ = 0;
+	int attaque_ = 0;
+	int coutBle_ = 0;
+	int coutBois_ = 0;
+	int coutGem_ = 0;*/
+	public float x_ = 0;
+	public float y_ = 0;
+	public Transform bitch;
+	GameObject card;
+	int i = 0;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnMouseDown()
+	{
+		Transform t = Instantiate(bitch, new Vector3 (0, 1, 0), Quaternion.Euler (new Vector3 (90, 180, 0))) as Transform;
+		card = t.gameObject;
+		i++;
+		card.name = "card" + i.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x_ = card.renderer.bounds.size.x;
+		y_ = card.renderer.bounds.size.y;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (x_, y_, x_ * 0.1f, y_ * 0.1f), "2");
+	}
+
+	/*public void setCarte()
+	{
+		nom_ = "Troll méchant";
+		vie_ = 5;
+		armure_ = 1;
+		attaque_ = 2;
+		coutBle_ = 2;
+		coutBois_ = 3;
+		coutGem_ = 0;
+	}*/
+}
