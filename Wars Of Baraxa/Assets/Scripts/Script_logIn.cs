7680074eb54a77b3a537f9c6e1c0fc03eb0017f8
OMimeault <olivierbmimeault@gmail.com>
2014-11-25 21:53:39 -0500

    card icons
    card above icons

--- Wars Of Baraxa/Assets/Scripts/Script_logIn.cs
+++ Wars Of Baraxa/Assets/Scripts/Script_logIn.cs
@@ -36,7 +36,7 @@ public class Script_logIn : MonoBehaviour {
         if (connexionServeur.sck == null)
         {
             connexionServeur.sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("172.17.104.114"), 1234);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("172.17.104.102"), 1234);
             try
             {
                 connexionServeur.sck.Connect(localEndPoint);
